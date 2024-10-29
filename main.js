bc1f0452297d9cdd839a9835e24c4dc86381800e
Vimal Raveendran <vimalraveendra@gmail.com>
2024-10-29 14:16:42 +0100

    added header section, slick carousel, modified variables,general sass styles

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".carousel-container").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 350,
+    dots: true,
+    prevArrow:
+      " <img src='./assets/arrow.svg'  alt='left-arrow' class='left-arrow'>",
+    nextArrow:
+      "<img src='./assets/arrow.svg' alt='right-arrow' class='right-arrow'>",
+  });
+});
